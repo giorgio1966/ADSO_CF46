a50dfdb3619db8e4bfb60aefad720eb3e1965bc4
giorgio1966 <jhaycald@hotmail.com>
2022-02-23 21:09:41 -0500

    segundo avance
    Segundo avance

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -126,7 +126,7 @@
           figure
             img(src='@/assets/template/tema1/arquitectura.svg', alt='Arquitectura tecnológica')
 
-    .titulo-segundo.color-acento-botones.mb-4
+    .titulo-segundo.color-acento-botones.mb-4.mt-4
       #manUser.h2 1.2 Manuales de usuario
     
     .row.mt-5.mb-5
@@ -155,43 +155,201 @@
             p Un manual de usuario se debe adecuar a la empresa que solicita el producto, los usuarios lo leen para encontrar consejos prácticos sobre cómo utilizar la solución de software o superar un problema cuando estos ocurren.
     p.mb-4  Para realizar un manual de usuario hay que tener en cuenta algunos consejos:
 
-  SlyderF.mb-5(columnas="col-lg-6 col-xl-4")
-    .tarjeta.bgBlanco.p-4
-      .row.justify-content-center.mb-3
-        .col-8
-          img(src='@/assets/componentes/tema1/pc.svg' alt='AvatarTop')
-      p.text-center Crear el manual de usuario, se deben escribir las instrucciones de forma clara y sencilla evitando palabras muy técnicas, mostrando todo lo que el usuario necesita saber, el diseño debe ser amigable y debe tener ayudas visuales.
+    SlyderF.mb-5(columnas="col-lg-6 col-xl-4")
+      .tarjeta.bgBlanco.p-4
+        .row.justify-content-center.mb-3
+          .col-5
+            img(src='@/assets/componentes/tema1/pc.svg' alt='AvatarTop')
+        p.text-center Crear el manual de usuario, se deben escribir las instrucciones de forma clara y sencilla evitando palabras muy técnicas, mostrando todo lo que el usuario necesita saber, el diseño debe ser amigable y debe tener ayudas visuales.
 
-    .tarjeta.bgBlanco.p-4
-      .row.justify-content-center.mb-3
-        .col-8
-          img(src='@/assets/componentes/tema1/libro.svg' alt='AvatarTop')
-      p.text-center Probar el manual, con personas que no conozcan el producto, con estas pruebas se puede enriquecer el manual, entrevistar a todos los desarrolladores, ellos conocen el producto mejor que nadie y pueden señalar las cosas que puede haber pasado por alto.
+      .tarjeta.bgBlanco.p-4
+        .row.justify-content-center.mb-3
+          .col-5
+            img(src='@/assets/componentes/tema1/libro.svg' alt='AvatarTop')
+        p.text-center Probar el manual, con personas que no conozcan el producto, con estas pruebas se puede enriquecer el manual, entrevistar a todos los desarrolladores, ellos conocen el producto mejor que nadie y pueden señalar las cosas que puede haber pasado por alto.
 
-    .tarjeta.bgBlanco.p-4
-      .row.justify-content-center.mb-3
-        .col-8
-          img(src='@/assets/componentes/tema1/hojas.svg' alt='AvatarTop')
-      p.text-center Mantener la documentación actualizada: Cuando hagan modificaciones en el producto, es necesario tener todo actualizado;  la documentación electrónica tiene una gran ventaja sobre la impresa,  incluso los documentos impresión pueden actualizarse para descargarlos   del sitio web de la empresa. 
+      .tarjeta.bgBlanco.p-4
+        .row.justify-content-center.mb-3
+          .col-5
+            img(src='@/assets/componentes/tema1/hojas.svg' alt='AvatarTop')
+        p.text-center Mantener la documentación actualizada: Cuando hagan modificaciones en el producto, es necesario tener todo actualizado;  la documentación electrónica tiene una gran ventaja sobre la impresa,  incluso los documentos impresión pueden actualizarse para descargarlos   del sitio web de la empresa. 
 
-    .tarjeta.bgBlanco.p-4
-      .row.justify-content-center.mb-3
-        .col-8
-          img(src='@/assets/componentes/tema1/tabla.svg' alt='AvatarTop')
-      p.text-center Planificar la información que se desea incluir, enfocarse en que se está ayudando al usuario de manera objetiva, también se debe tener en cuenta que otros recursos se pone a disposición del usuario y quienes son los responsables de elaborar el documento y de su aprobación.
+      .tarjeta.bgBlanco.p-4
+        .row.justify-content-center.mb-3
+          .col-5
+            img(src='@/assets/componentes/tema1/tabla.svg' alt='AvatarTop')
+        p.text-center Planificar la información que se desea incluir, enfocarse en que se está ayudando al usuario de manera objetiva, también se debe tener en cuenta que otros recursos se pone a disposición del usuario y quienes son los responsables de elaborar el documento y de su aprobación.
 
-    .tarjeta.bgBlanco.p-4
-      .row.justify-content-center.mb-3
-        .col-8
-          img(src='@/assets/componentes/tema1/hojas2.svg' alt='AvatarTop')
-      p.text-center Seleccionar el formato para crear el manual puede ser, .doc, .txt  ,html, .pdf, .csv, .xls etc;  por lo general la mayoría de manuales se imprimen pero es recomendable tener una versión electrónica en el sitio web, si no lo tiene, debe pensar en donde puede alojar esta información ya que en la actualidad la gran mayoría de personas acceden a la información desde su dispositivo móvil o Smart Phone. 
+      .tarjeta.bgBlanco.p-4
+        .row.justify-content-center.mb-3
+          .col-5
+            img(src='@/assets/componentes/tema1/hojas2.svg' alt='AvatarTop')
+        p.text-center Seleccionar el formato para crear el manual puede ser, .doc, .txt  ,html, .pdf, .csv, .xls etc;  por lo general la mayoría de manuales se imprimen pero es recomendable tener una versión electrónica en el sitio web, si no lo tiene, debe pensar en donde puede alojar esta información ya que en la actualidad la gran mayoría de personas acceden a la información desde su dispositivo móvil o Smart Phone. 
 
-    .tarjeta.bgBlanco.p-4
-      .row.justify-content-center.mb-3
-        .col-8
-          img(src='@/assets/componentes/tema1/pc2.svg' alt='AvatarTop')
-      p.text-center Tener en cuenta a los lectores, el tipo de población a la cual está dirigida la solución informática. 
+      .tarjeta.bgBlanco.p-4
+        .row.justify-content-center.mb-3
+          .col-5
+            img(src='@/assets/componentes/tema1/pc2.svg' alt='AvatarTop')
+        p.text-center Tener en cuenta a los lectores, el tipo de población a la cual está dirigida la solución informática. 
+    p.mb-4.mt-4 Recuerde, su contenido solo es excelente si es preciso, asegúrese que lo que ofrece a sus clientes realmente les ayude a utilizar su producto. Pero por lo general debe tener la siguiente estructura:
+    .row.justify-content-center.mb-4
+      .col-8.p-4.bgPrimario
+        .row
+          .col-6
+            ul.lista-ul.mt-5.ms-5
+              li 
+                i.fas.fa-check.text-secundario
+                | Tabla de contenido.
+              li
+                i.fas.fa-check.text-secundario
+                | Introducción
+              li 
+                i.fas.fa-check.text-secundario
+                | Cuerpo “el manual en sí”.
+              li 
+                i.fas.fa-check.text-secundario
+                | Flujograma           
+              li 
+                i.fas.fa-check.text-secundario
+                | Glosario de terminología
+              li
+                i.fas.fa-check.text-secundario
+                | Conclusiones
+              li 
+                i.fas.fa-check.text-secundario
+                | Recomendaciones
+              li 
+                i.fas.fa-check.text-secundario
+                | Contactos de los desarrolladores. 
+          .col-6.p-5
+            img(src="@/assets/curso/tema1/figura1.png")
+    .titulo-segundo.color-acento-botones.mb-4.mt-4
+      #manInt.h2 1.3 Manuales de instalación
+    .row.mb-4
+      .col-5
+        img(src="@/assets/curso/tema1/imagen3.jpg")
+      .col-7
+        .row.mb-2.bgAcentoBotones
+          .col-12
+            .row
+             .col-1
+               i.fas.fa-quote-left.Text-grande.text-primario.mt-3
+            .row
+              .col-1
+              .col-10
+                p.ms-5 - Según UGUR (2009)
+                p.mt-2.mb-2.ms-5 Estos manuales sirven para describir la instalación de un sistema mecánico, electrónico o un producto de software, su fin principal es instruir sobre las tareas de configuración e instalación.
+            .row
+              .col-11
+              .col-1
+                p.i.fas.fa-quote-right.Text-grande.text-primario.mb-4       
+        p.mb-4.mt-4 Estos son los componentes principales de una guía de instalación de software:
+    .row
+      .col-4.mb-4.mt-4.bgFondoTitulo
+        p.text-white.p-3.mb-0.text-bold Requisitos del sistema 
+    p.mb-4 Aquí se debe detallar qué tipo de hardware o firmware se necesita para instalar este software, se debe especificar si funciona en un sistema operativo Mac OS X o Windows 10, según Microsoft (2021) los requisitos mínimos para instalar el sistema operativo Windows 11, son los siguientes:
 
+
+    TabsA.color-acento-botones.mb-5
+      .tarjeta.color-acento-botones.p-4(titulo="Procesador")
+        h4.mt-4 Procesador
+        p.mb-4 1 giga Hertz (GHz) o más rápido con 2 o más núcleos en un procesador de 64 bits compatible o sistema en un chip (SoC).
+        .row.justify-content-center
+          .col-md-10
+            img(src="@/assets/curso/tema1/procesador.jpg")
+
+      .tarjeta.color-acento-botones.p-4(titulo="RAM")
+        h4.mt-4 RAM
+        p.mb-4 4 gigabytes (GB).
+        .row.justify-content-center
+          .col-md-10
+            img(src="@/assets/curso/tema1/RAM.jpg")
+      .tarjeta.color-acento-botones.p-4(titulo="Almacenamiento")
+        h4.mt-4 Almacenamiento
+        p.mb-4 Dispositivo de almacenamiento de 64 GB o más. Nota: Para más información, consulta abajo en "Más información sobre los requisitos de espacio de almacenamiento para mantener Windows 11 actualizado".
+        .row.justify-content-center
+          .col-md-10
+            img(src="@/assets/curso/tema1/almacen1.jpg")
+      .tarjeta.color-acento-botones.p-4(titulo="Firmware del sistema")
+        h4.mt-4 Firmware del sistema
+        p.mb-4 UEFI, compatible con Arranque seguro. Consulta aquí para obtener información sobre cómo tu PC puede cumplir este requisito.
+        .row.justify-content-center
+          .col-md-10
+            img(src="@/assets/curso/tema1/firmware.jpg")
+      .tarjeta.color-acento-botones.p-4(titulo="Tarjeta gráfica")
+        h4.mt-4 Tarjeta gráfica
+        p.mb-4 Compatible con DirectX 12 o posterior con controlador WDDM 2.0.
+        .row.justify-content-center
+          .col-md-10
+            img(src="@/assets/curso/tema1/tarjeta.jpg")
+      .tarjeta.color-acento-botones.p-4(titulo="Pantalla")
+        h4.mt-4 Pantalla
+        p.mb-4 Pantalla de alta definición (720p) de más de 9" en diagonal, con canal de 8 bits por color.
+        .row.justify-content-center
+          .col-md-10
+            img(src="@/assets/curso/tema1/pantalla.jpg")
+      .tarjeta.color-acento-botones.p-4(titulo="Conexión a Internet y cuenta de Microsoft")
+        h4.mt-4 Conexión a Internet y cuenta de Microsoft
+        p.mb-4 Windows 11 Home requiere conexión a Internet y una cuenta de Microsoft. El cambio fuera de Windows 11 en modo S también requiere conexión a Internet. Obtén más información sobre el modo S aquí. Para todas las ediciones de Windows 11, se requiere acceso a Internet para poder realizar actualizaciones, así como para descargar y aprovechar algunas de las  características. Para algunas características se requiere una cuenta de Microsoft.
+      .tarjeta.color-acento-botones.p-4(titulo="Almacenamiento")
+        h4.mt-4 Almacenamiento
+        p.mb-4 Dispositivo de almacenamiento de 64 GB o más. Nota: Para más información, consulta abajo en "Más información sobre los requisitos de espacio de almacenamiento para mantener Windows 11 actualizado".
+        .row.justify-content-center
+          .col-md-10
+            img(src="@/assets/curso/tema1/almacen2.jpg")
+      .tarjeta.color-acento-botones.p-4(titulo="TPM")
+        h4.mt-4 TPM
+        p Módulo de plataforma segura (TPM) versión 2.0. Consulta aquí para ver instrucciones sobre cómo se puede habilitar tu PC para que cumpla este requisito.  p
+        p Windows 11 Home requiere conexión a Internet y una cuenta de Microsoft. Para todas las ediciones de Windows 11, se requiere acceso a Internet para poder realizar actualizaciones, así como para descargar y aprovechar algunas de las características. Para algunas características se requiere una cuenta de Microsoft.
+        .row.justify-content-center
+          .col-md-10
+            img(src="@/assets/curso/tema1/TPM.jpg")
+    img(src="@/assets/curso/tema1/manos.jpg")
+
+    TabsC.color-acento-botones
+      .py-3.py-md-4(titulo="Descripción general de las características del sistema o del producto")
+        .row.align-items-center
+          .col-md-6
+            figure
+              img(src='@/assets/template/tema1/hombreSentado.png', alt='Características del sistema')
+          .col-md-6.mb-4.mb-md-0
+            h4 Descripción general de las características del sistema o del producto
+            p Aquí se describen cuáles son las características básicas del software o producto en cuestión, por lo general se usa una lista o en una tabla. Ejemplo, en un software de telefonía IP (comunicación), se debe indicar cuántas líneas admite el software, cuántos usuarios y contraseñas admite, el tipo de base de datos que necesita, cuántas zonas o particiones soporta, etc.
+            .row.mb-2.bgAcentoBotones
+              .col-12
+                .row
+                .col-1
+                  i.fas.fa-quote-left.Text-grande.text-primario.mt-3
+                .row
+                  .col-1
+                  .col-10
+                    p.ms-5 - De acuerdo a UGUR (2009)
+                    p.mt-2.mb-2.ms-5 siempre son importantes los datos de contacto del desarrollador o de la empresa que creó la solución informática o con el departamento de atención al cliente, dado el caso que se presente alguna duda relacionada con el producto, garantía, registro, etc.
+                .row
+                  .col-11
+                  .col-1
+                    p.i.fas.fa-quote-right.Text-grande.text-primario.mb-4
+            p.mb-4 También en este apartado se incluye cualquier exención de responsabilidad legal, divulgaciones, advertencias de agencias gubernamentales oficiales, información de registro, etc.
+
+      .py-3.py-md-4(titulo="Configuraciones de instalación")
+        .row.align-items-center
+          .col-md-6.mb-4.mb-md-0
+            figure
+              img(src='@/assets/template/tema1/hombreCaminando.png', alt='Configuraciones de instalación')
+          .col-md-6
+            h4 Configuraciones de instalación
+            p Es común que un software de aplicación o tenga una instalación "típica" y una "excepcional" o "diseñada por el cliente". En ocasiones, esta instalación también se denominará "implementación" en la documentación del producto.
+        
+      .py-3.py-md-4(titulo="Configuración mínima de inicio rápido")
+        .row.align-items-center
+          .col-md-6.mb-4.mb-md-0
+            figure
+              img(src='@/assets/template/tema1/hombrePC.png', alt='Configuración mínima de inicio rápido')
+          .col-md-6
+            h4.mt-5 Configuración mínima de inicio rápido
+            p En algunos software se publica una guía de "Inicio rápido" independiente, además se explica cómo identificar e interpretar cualquier sistema o códigos de hardware y números de serie que vienen con el producto.
+            p Se deben incluir gráficos del paso a paso de cómo instalar el software, también se muestra las conexiones de cableado correctas, si las hay. Si se trata de alguna instalación física.
+            p Si se realiza todas las sugerencias en este manual se fomentará la lealtad del cliente, generar altos niveles de satisfacción y minimizar las llamadas al departamento de atención al cliente.
 
 </template>
 
--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -3,138 +3,71 @@
   BannerInterno
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5
     
-    .titulo-principal
-      .h3 Producción Audiovisual
+    .titulo-principal.color-acento-contenido
+      .titulo-principal__numero
+        span 1
+      h1 Capacitación de usuarios
     
-    #t_2_1.titulo-segundo
-      .h4 2.1 Infografía Animada
-    
-    p.mb-4 Propuesta infográfica en animación que refuerce un tema y matice conceptos dados de manera creativa y con intención pedagógica.
+    .row.bgFondoUnosyCerosAzul.mt-4.justify-content-center.p-5
+      .col-md-10
+        img(src='@/assets/curso/tema2/reunionVirtual.jpg')
+        p.mt-5.mb-4 La tecnología cambia a pasos agigantados, esto se puede ver reflejado en la creación y nuevas versiones de   programas, por lo cual la clave es saber cómo capacitar a los empleados sobre los nuevos sistemas o tecnología que se implementan, y hay que hacerlo de manera efectiva y eficaz para lograr una correcta transferencia del conocimiento.
 
-    figure.mb-5
-      .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2cnKTwQ85fg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+    .titulo-segundo.color-acento-botones.mb-4
+      #t_2_1_Tec.h2 2.1 Técnicas
+      
+    SlyderA
+      .row.bgFondoPantalla.p-5.m-5
+        .col-md-12.mb-4.mb-md-0.p-5
+          h3.mt-4.mb-4.ms-4 Los instructores deben conocer muy bien el software
+          .row
+            .col-md-6.p-2
+              p.p-2.ms-4 Los formadores deben ser expertos en el manejo del programa, esto transmite confianza y entusiasmo a los usuarios de la capacitación.
+              p.ms-2.mb-2.me-2.ms-4 Al diseñar la capacitación debe contar con personal profesional en la materia, los cuales tengan facilidad de expresión y buen manejo del público.   
+            .col-md-6.p-2
+              figure
+                img(src='@/assets/template/tema2/reunion.jpg', alt='Los instructores deben conocer muy bien el software')
+      .row.bgFondoPantalla.p-5.m-5
+        .col-md-12.mb-4.mb-md-0.p-5
+          h3.mt-4.mb-4.ms-4 Los instructores deben conocer muy bien el software
+          .row
+            .col-md-6.p-2
+              p.p-2.ms-4 Si la capacitación es de manera virtual o con video tutoriales asegurase de contratar personas expertas en la creación de estos recursos ya que la presentación y la calidad es muy importante. 
+            .col-md-6.p-2
+              figure
+                img(src='@/assets/template/tema2/PC.jpg', alt='Los instructores deben conocer muy bien el software')
 
-    #t_2_2.titulo-segundo
-      .h4 2.2 Video Motion
+      .row.bgFondoPantalla.p-5.m-5
+        .col-md-12.mb-4.mb-md-0.p-5
+          h3.mt-4.mb-4.ms-4 Hacer de la formación una prioridad
+          .row
+            .col-md-6.p-2
+              p.p-2.ms-4 Hay que establecer acuerdos con los jefes de personal o con los propietarios de las empresas para establecer la capacitación de manera obligatoria, esto evita problemas inconvenientes a futuro como volver a agendar capacitaciones y problemas por incorrecto manejo de software o aplicación.
+            .col-md-6.p-2
+              figure
+                img(src='@/assets/template/tema2/manosPC.jpg', alt='Hacer de la formación una prioridad')
 
-    p.mb-4 Animación digital que crea la ilusión de movimiento mediante imágenes, fotografías, títulos, colores y diseños.
+      .row.bgFondoPantalla.p-5.m-5
+        .col-md-12.mb-4.mb-md-0.p-5
+          h3.mt-4.mb-4.ms-4 Utilizar incentivos en la capacitación
+          .row
+            .col-md-6.p-2
+              p.p-2.ms-4 Los incentivos motivan y comprometen a los participantes creando un ambiente cálido en el proceso de capacitación, la clave es conocer bien a los empleados por intermedio de los jefes, para algunos vasta con un reconocimiento verbal por su dedicación, para otros puede ser un buen refrigerio o golosinas, también se puede hablar con los dueños de la empresa para que los usuarios puedan ser recompensados con hora libres.
+            .col-md-6.p-2
+              figure
+                img(src='@/assets/template/tema2/estudiantes.jpg', alt='Utilizar incentivos en la capacitación')
 
-    figure.mb-5
-      .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/E7-BaX6ox9s" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+      .row.bgFondoPantalla.p-5.m-5
+        .col-md-12.mb-4.mb-md-0.p-5
+          h3.mt-4.mb-4.ms-4 Elegir un formato adecuado para la capacitación
+          .row
+            .col-md-6.p-2
+              p.p-2.ms-4 En la mayoría de casos las capacitaciones o conferencias son de manera magistral y de poco tiempo, lo cual no es efectivo para ayudar a los usuarios a desarrollar sus nuevas habilidades y que los aplique con eficacia, hay que ser creativos, didácticos y utilizar pausas activas para evitar el desgaste de los usuarios, buscando la aplicabilidad del aprendizaje. 
+            .col-md-6.p-2
+              figure
+                img(src='@/assets/template/tema2/presentacion.jpg', alt='Elegir un formato adecuado para la capacitación')
 
-    #t_2_3.titulo-segundo
-      .h4 2.3 Motion + Presentador
 
-    p.mb-4 Es el uso locutor + acompañamiento de imágenes como fondo, el estilo tiene una mezcla entre imagen viva y recursos gráficos vectoriales  se usa cuando un material tiene información de datos numéricos o gráficos de valores. 
-
-    figure.mb-5
-      .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/Ky73iaXuuTA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
-
-    #t_2_4.titulo-segundo
-      .h4 2.4 Animación 2D
-
-    p.mb-4 Es el uso locutor + acompañamiento de imágenes como fondo, el estilo tiene una mezcla entre imagen viva y recursos gráficos vectoriales  se usa cuando un material tiene información de datos numéricos o gráficos de valores. 
-
-    figure.mb-5
-      .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/3b8VctSm558" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
-
-    #t_2_5.titulo-segundo
-      .h4 2.5 Video interactivo
-
-    p.mb-4 Visualización de videos con diferentes tipos de preguntas o anotaciones.
-
-    figure.mb-5
-      .video
-        iframe(src="https://vizia.co/videos/94d994c5fe93f1b08a60eb/embed" allowtransparency="true" frameborder="0" scrolling="no" width="640" height="390")
-
-    #t_2_6.titulo-segundo
-      .h4 2.6 Infografías
-
-    p.mb-4 Es una herramienta de comunicación de alta precisión, el cual tiene una narrativa gráfica con un texto corto acompañada de elementos visuales que tienen como intención de captar rápidamente la atención del lector. 
-
-    figure.mb-5
-      img(src='@/assets/curso/infografia-1.jpg', alt='Infografía Fundamentos del Servicio de Interpretación')
-      figcaption Fundamentos del Servicio de Interpretación
-
-    .tarjeta.tarjeta--morada.p-3.mb-5
-      .row.justify-content-around.align-items-center
-        .col
-          .row.justify-content-between.align-items-center
-            .col.mb-3.mb-sm-0
-              .h5.mb-0 Fundamentos del Servicio de Interpretación
-              p Formato PDF
-            .col-sm-auto
-              a.boton.boton--b(:href="obtenerLink('/downloads/fundamentos_del_servicio_de_Interpretacion.pdf')" target="_blank")
-                span Descargar
-                i.fas.fa-file-download
-    
-    figure.mb-5
-      img(src='@/assets/curso/infografia-2.jpg', alt='Infografía Plan de Negocios')
-      figcaption Plan de Negocios
-
-    .tarjeta.tarjeta--morada.p-3.mb-5
-      .row.justify-content-around.align-items-center
-        .col
-          .row.justify-content-between.align-items-center
-            .col.mb-3.mb-sm-0
-              .h5.mb-0 Plan de Negocios
-              p Formato PDF
-            .col-sm-auto
-              a.boton.boton--b(:href="obtenerLink('/downloads/plan_de_negocios.pdf')" target="_blank")
-                span Descargar
-                i.fas.fa-file-download
-    
-    figure.mb-5
-      img(src='@/assets/curso/infografia-3.jpg', alt='Infografía Cobertura poblacional')
-      figcaption Cobertura poblacional
-
-    .tarjeta.tarjeta--morada.p-3.mb-5
-      .row.justify-content-around.align-items-center
-        .col
-          .row.justify-content-between.align-items-center
-            .col.mb-3.mb-sm-0
-              .h5.mb-0 Cobertura poblacional
-              p Formato PDF
-            .col-sm-auto
-              a.boton.boton--b(:href="obtenerLink('/downloads/cobertura_poblacional.pdf')" target="_blank")
-                span Descargar
-                i.fas.fa-file-download
-    
-    figure.mb-5
-      img(src='@/assets/curso/infografia-4.jpg', alt='Infografía Historia de las microfinanzas: Mohammad Yunus')
-      figcaption Historia de las microfinanzas: Mohammad Yunus
-
-    .tarjeta.tarjeta--morada.p-3.mb-5
-      .row.justify-content-around.align-items-center
-        .col
-          .row.justify-content-between.align-items-center
-            .col.mb-3.mb-sm-0
-              .h5.mb-0 Historia de las microfinanzas: Mohammad Yunus
-              p Formato PDF
-            .col-sm-auto
-              a.boton.boton--b(:href="obtenerLink('/downloads/historia_de_las_microfinanzas_mohammad_yunus.pdf')" target="_blank")
-                span Descargar
-                i.fas.fa-file-download
-    
-    figure.mb-5
-      img(src='@/assets/curso/infografia-5.jpg', alt='Infografía Psicología de la moda: sexualidad, ropa mágica, ropa malévola y ropa neurótica')
-      figcaption Psicología de la moda: sexualidad, ropa mágica, ropa malévola y ropa neurótica
-
-    .tarjeta.tarjeta--morada.p-3.mb-5
-      .row.justify-content-around.align-items-center
-        .col
-          .row.justify-content-between.align-items-center
-            .col.mb-3.mb-sm-0
-              .h5.mb-0 Psicología de la moda: sexualidad, ropa mágica, ropa malévola y ropa neurótica
-              p Formato PDF
-            .col-sm-auto
-              a.boton.boton--b(:href="obtenerLink('/downloads/psicologia_de_la_moda.pdf')" target="_blank")
-                span Descargar
-                i.fas.fa-file-download
 
 </template>
 
